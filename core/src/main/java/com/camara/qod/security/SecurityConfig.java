87a2b154623809f7061cf181714440154939775c
Maximilian Laue <Maximilian.Laue@t-systems.com>
2023-01-06 23:17:09 +0100

    feat: allow requests against swagger - & actuator-endpoints without token

--- core/src/main/java/com/camara/qod/security/SecurityConfig.java
+++ core/src/main/java/com/camara/qod/security/SecurityConfig.java
@@ -24,12 +24,24 @@ package com.camara.qod.security;
 
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @ConditionalOnProperty(value = "enhanced-token-validation.enabled")
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorizeRequest -> {
+      authorizeRequest.antMatchers("/swagger-ui/**").permitAll();
+      authorizeRequest.antMatchers("/v3/api-docs/**").permitAll();
+      authorizeRequest.antMatchers("/actuator/**").permitAll();
+    }).csrf().disable();
+    return http.build();
+  }
 }
